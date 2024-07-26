a18448a02f190e3478d39ede14fa70e531e3e742
NarasimhaProjects <narasimhareddy.mba1996@gmail.com>
2024-07-26 17:13:48 +0530

    Files updated

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'https://taskmernbackend-nabendubiswas.b4a.run/api/users/'
+const API_URL = 'https://task-mern-backend-g8jj.onrender.com/api/users/'
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData)
